c40321a5e72f434e50df2791a68b6b049d8cecef
Nguyễn Chí Văn <vannc@vincss.local>
2021-07-02 16:00:00 +0700

    update verification flow

--- src/client/client.ts
+++ src/client/client.ts
@@ -348,20 +348,24 @@ export class Fido2Client implements IFido2Client {
                     if (!uv && userVerification === 'discouraged') return resolve({ userVerification: false, pinUvAuthToken: undefined });
 
                     /**
-                     * Built-in user verification not configured and relying party require user verification,
+                     * Built-in user verification has been configured.
                      * let user to configure built-in user verification.
                      */
-                    if (!uv && userVerification !== 'discouraged') {
+                    if (uv) return this.internalGetPinUvAuthToken(uv, clientPin, pinUvAuthToken, info.version).then(token => {
+                        if (uv) resolve({ userVerification: token === undefined, pinUvAuthToken: token });
+                    });
+
+
+                    if (!uv) {
                         /**
                          * @TODO implement built-in user verification configure.
                          */
-                        throw new MethodNotImplemented();
+                        // throw new MethodNotImplemented();
                     }
 
-                    this.internalGetPinUvAuthToken(uv, clientPin, pinUvAuthToken, info.version).then(token => {
-                        if (uv) resolve({ userVerification: token === undefined, pinUvAuthToken: token });
-                    });
-                    return;
+                    /**
+                     * Fall back to client pin.
+                     */
                 }
 
                 if (clientPin !== undefined) {
